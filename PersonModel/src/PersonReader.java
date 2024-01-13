763c5693d7b56b68afcee16c6afaf4484832f1ea
IanImsicke <156134508+IanImsicke@users.noreply.github.com>
2024-01-12 20:42:43 -0500

    Add files via upload

---
+++ PersonModel/src/PersonReader.java
@@ -0,0 +1,75 @@
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+
+import static java.nio.file.StandardOpenOption.CREATE;
+import javax.swing.JFileChooser;
+
+public class PersonReader
+{
+    public static void main(String[] args)
+    {
+        JFileChooser chooser = new JFileChooser();
+        File selectedFile;
+        String rec = "";
+
+        try {
+
+
+
+            File workingDirectory = new File(System.getProperty("user.dir"));
+
+
+            chooser.setCurrentDirectory(workingDirectory);
+
+
+            if (chooser.showOpenDialog(null) == JFileChooser.APPROVE_OPTION) {
+                selectedFile = chooser.getSelectedFile();
+                Path file = selectedFile.toPath();
+
+                InputStream in =
+                        new BufferedInputStream(Files.newInputStream(file, CREATE));
+                BufferedReader reader =
+                        new BufferedReader(new InputStreamReader(in));
+
+
+                int line = 0;
+                System.out.println("       ID#   Firstname Lastname Title YOB");
+                System.out.println("----------------------------------------------------");
+                while (reader.ready()) {
+                    rec = reader.readLine();
+                    line++;
+                    // echo to screen
+
+                    System.out.printf("\n%5s %-8s ", line, rec);
+                    //System.out.printf("\nLine %4d %-60s ", line, rec);
+                }
+                reader.close(); // must close the file to seal it and flush buffer
+                System.out.println("\n\nData file read!");
+
+
+            } else  // User closed the chooser without selecting a file
+            {
+                System.out.println("No file selected!!! ... exiting.\nRun the program again and select a file.");
+            }
+        }
+        catch (FileNotFoundException e)
+        {
+            System.out.println("File not found!!!");
+            e.printStackTrace();
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+
+
+    }
+}
